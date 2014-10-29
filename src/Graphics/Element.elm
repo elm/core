311089465d5f54520b7d952e5a3cd1f771c90e9e
Evan Czaplicki <info@elm-lang.org>
2014-10-29 10:36:56 -0700

    cosmetic: clean up indentation

--- src/Graphics/Element.elm
+++ src/Graphics/Element.elm
@@ -43,6 +43,7 @@ import List as List
 import Color (..)
 import Maybe ( Maybe(..) )
 
+
 type alias Properties = {
   id      : Int,
   width   : Int,
@@ -55,10 +56,12 @@ type alias Properties = {
   click   : ()
  }
 
+
 type alias Element =
     { props : Properties
     , element : ElementPrim
     }
+
 
 {-| An Element that takes up no space. Good for things that appear conditionally:
 
@@ -67,46 +70,83 @@ type alias Element =
 empty : Element
 empty = spacer 0 0
 
+
 {-| Get the width of an Element -}
 widthOf : Element -> Int
-widthOf e = e.props.width
+widthOf e =
+    e.props.width
+
 
 {-| Get the height of an Element -}
 heightOf : Element -> Int
-heightOf e = e.props.height
+heightOf e =
+    e.props.height
+
 
 {-| Get the width and height of an Element -}
 sizeOf : Element -> (Int,Int)
-sizeOf e = (e.props.width, e.props.height)
+sizeOf e =
+    (e.props.width, e.props.height)
+
 
 {-| Create an `Element` with a given width. -}
 width : Int -> Element -> Element
 width nw e =
     let p = e.props
-        props = case e.element of
-                  Image _ w h _ -> {p| height <- round (toFloat h / toFloat w * toFloat nw) }
-                  RawHtml       -> {p| height <- snd (Native.Utils.htmlHeight nw e.element) }
-                  _ -> p
-    in { element=e.element, props={ props | width <- nw } }
+
+        props =
+            case e.element of
+              Image _ w h _ ->
+                  { p |
+                      height <- round (toFloat h / toFloat w * toFloat nw)
+                  }
+
+              RawHtml ->
+                  { p |
+                      height <- snd (Native.Utils.htmlHeight nw e.element)
+                  }
+
+              _ -> p
+    in
+        { element = e.element
+        , props = { props | width <- nw }
+        }
+
 
 {-| Create an `Element` with a given height. -}
 height : Int -> Element -> Element
 height nh e =
     let p = e.props
-        props = case e.element of
-                  Image _ w h _ -> {p| width <- round (toFloat w / toFloat h * toFloat nh) }
-                  _ -> p
-    in { element=e.element, props={ p | height <- nh} }
+        props =
+            case e.element of
+              Image _ w h _ ->
+                  { p |
+                      width <- round (toFloat w / toFloat h * toFloat nh)
+                  }
+              _ -> p
+    in
+        { element = e.element
+        , props = { p | height <- nh }
+        }
+
 
 {-| Create an `Element` with a new width and height. -}
 size : Int -> Int -> Element -> Element
-size w h e = height h (width w e)
+size w h e =
+    height h (width w e)
+
 
 {-| Create an `Element` with a given opacity. Opacity is a number between 0 and 1
 where 0 means totally clear.
 -}
 opacity : Float -> Element -> Element
-opacity o e = let p = e.props in { element=e.element, props={p| opacity <- o} }
+opacity o e =
+    let p = e.props
+    in
+        { element = e.element
+        , props = { p | opacity <- o }
+        }
+
 
 {-| Create an `Element` with a given background color. -}
 color : Color -> Element -> Element
@@ -115,23 +155,35 @@ color c e = let p = e.props in
             , props = { p | color <- Just c}
             }
 
+
 {-| Create an `Element` with a tag. This lets you link directly to it.
 The element `(tag "all-about-badgers" thirdParagraph)` can be reached
 with a link like this: `/facts-about-animals.elm#all-about-badgers`
 -}
 tag : String -> Element -> Element
-tag  name e = let p = e.props in
-              { element=e.element, props={p | tag <- name} }
+tag name e =
+    let p = e.props
+    in
+        { element = e.element
+        , props = { p | tag <- name }
+        }
+
 
 {-| Create an `Element` that is a hyper-link. -}
 link : String -> Element -> Element
-link href e = let p = e.props in
-              { element=e.element, props={p | href <- href} }
+link href e =
+    let p = e.props
+    in
+        { element = e.element
+        , props = { p | href <- href }
+        }
+
 
 newElement w h e =
   { props = Properties (Native.Utils.guid ()) w h 1 Nothing "" "" () ()
   , element = e
   }
+
 
 type ElementPrim
     = Image ImageStyle Int Int String
@@ -143,15 +195,20 @@ type ElementPrim
 
 type ImageStyle = Plain | Fitted | Cropped (Int,Int) | Tiled
 
+
 {-| Create an image given a width, height, and image source. -}
 image : Int -> Int -> String -> Element
-image w h src = newElement w h (Image Plain w h src)
+image w h src =
+    newElement w h (Image Plain w h src)
+
 
 {-| Create a fitted image given a width, height, and image source.
 This will crop the picture to best fill the given dimensions.
 -}
 fittedImage : Int -> Int -> String -> Element
-fittedImage w h src = newElement w h (Image Fitted w h src)
+fittedImage w h src =
+    newElement w h (Image Fitted w h src)
+
 
 {-| Create a cropped image. Take a rectangle out of the picture starting
 at the given top left coordinate. If you have a 140-by-140 image,
@@ -167,14 +224,18 @@ tiledImage : Int -> Int -> String -> Element
 tiledImage w h src =
     newElement w h (Image Tiled w h src)
 
+
 type Three = P | Z | N
+
 type Pos = Absolute Int | Relative Float
+
 type alias Position =
     { horizontal : Three
     , vertical : Three
     , x : Pos
     , y : Pos
     }
+
 
 {-| Put an element in a container. This lets you position the element really
 easily, and there are tons of ways to set the `Position`.
@@ -185,15 +246,20 @@ To center `element` exactly in a 300-by-300 square you would say:
 By setting the color of the container, you can create borders.
 -}
 container : Int -> Int -> Position -> Element -> Element
-container w h pos e = newElement w h (Container pos e)
+container w h pos e =
+    newElement w h (Container pos e)
+
 
 {-| Create an empty box. This is useful for getting your spacing right and
 for making borders.
 -}
 spacer : Int -> Int -> Element
-spacer w h = newElement w h Spacer
+spacer w h =
+    newElement w h Spacer
+
 
 type Direction = DUp | DDown | DLeft | DRight | DIn | DOut
+
 
 {-| Have a list of elements flow in a particular direction.
 The `Direction` starts from the first element in the list.
@@ -219,32 +285,39 @@ flow dir es =
     DIn    -> newFlow (List.maximum ws) (List.maximum hs)
     DOut   -> newFlow (List.maximum ws) (List.maximum hs)
 
+
 {-| Stack elements vertically.
 To put `a` above `b` you would say: ``a `above` b``
 -}
 above : Element -> Element -> Element
 above hi lo =
-    newElement (max (widthOf hi) (widthOf lo))
-               (heightOf hi + heightOf lo)
-               (Flow DDown [hi,lo])
+    newElement
+        (max (widthOf hi) (widthOf lo))
+        (heightOf hi + heightOf lo)
+        (Flow DDown [hi,lo])
+
 
 {-| Stack elements vertically.
 To put `a` below `b` you would say: ``a `below` b``
 -}
 below : Element -> Element -> Element
 below lo hi =
-    newElement (max (widthOf hi) (widthOf lo))
-               (heightOf hi + heightOf lo)
-               (Flow DDown [hi,lo])
+    newElement
+        (max (widthOf hi) (widthOf lo))
+        (heightOf hi + heightOf lo)
+        (Flow DDown [hi,lo])
+
 
 {-| Put elements beside each other horizontally.
 To put `a` beside `b` you would say: ``a `beside` b`` 
 -}
 beside : Element -> Element -> Element
 beside lft rht =
-    newElement (widthOf lft + widthOf rht)
-               (max (heightOf lft) (heightOf rht))
-               (Flow right [lft,rht])
+    newElement
+        (widthOf lft + widthOf rht)
+        (max (heightOf lft) (heightOf rht))
+        (Flow right [lft,rht])
+
 
 {-| Layer elements on top of each other, starting from the bottom:
 `layers == flow outward`
@@ -253,7 +326,8 @@ layers : [Element] -> Element
 layers es = 
   let ws = List.map widthOf es
       hs = List.map heightOf es
-  in  newElement (List.maximum ws) (List.maximum hs) (Flow DOut es)
+  in
+      newElement (List.maximum ws) (List.maximum hs) (Flow DOut es)
 
 
 -- Repetitive things --
