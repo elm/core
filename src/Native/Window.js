d06b24fdef6de8938aabeff07af23d06c8660ca0
Evan Czaplicki <info@elm-lang.org>
2014-11-04 15:25:02 -0800

    style: improve naming and indentation

--- src/Native/Window.js
+++ src/Native/Window.js
@@ -1,57 +1,62 @@
+Elm.Native = Elm.Native || {};
 Elm.Native.Window = {};
-Elm.Native.Window.make = function(elm) {
+Elm.Native.Window.make = function(localRuntime) {
 
-  elm.Native = elm.Native || {};
-  elm.Native.Window = elm.Native.Window || {};
-  if (elm.Native.Window.values) return elm.Native.Window.values;
+    localRuntime.Native = localRuntime.Native || {};
+    localRuntime.Native.Window = localRuntime.Native.Window || {};
+    if (localRuntime.Native.Window.values) {
+        return localRuntime.Native.Window.values;
+    }
 
-  var Signal = Elm.Signal.make(elm);
-  var NS = Elm.Native.Signal.make(elm);
-  var Tuple2 = Elm.Native.Utils.make(elm).Tuple2;
+    var Signal = Elm.Signal.make(localRuntime);
+    var NS = Elm.Native.Signal.make(localRuntime);
+    var Tuple2 = Elm.Native.Utils.make(localRuntime).Tuple2;
 
-  function getWidth() { return elm.node.clientWidth; }
-  function getHeight() {
-      if (elm.display === ElmRuntime.Display.FULLSCREEN) {
-          return window.innerHeight;
-      }
-      return elm.node.clientHeight;
-  }
+    function getWidth() {
+        return localRuntime.node.clientWidth;
+    }
+    function getHeight() {
+        if (localRuntime.display === ElmRuntime.Display.FULLSCREEN) {
+            return window.innerHeight;
+        }
+        return localRuntime.node.clientHeight;
+    }
 
-  var dimensions = NS.input(Tuple2(getWidth(), getHeight()));
-  dimensions.defaultNumberOfKids = 2;
+    var dimensions = NS.input(Tuple2(getWidth(), getHeight()));
+    dimensions.defaultNumberOfKids = 2;
 
-  // Do not move width and height into Elm. By setting the default number of kids,
-  // the resize listener can be detached.
-  var width  = A2(Signal.lift, function(p){return p._0;}, dimensions);
-  width.defaultNumberOfKids = 0;
+    // Do not move width and height into Elm. By setting the default number of kids,
+    // the resize listener can be detached.
+    var width  = A2(Signal.lift, function(p){return p._0;}, dimensions);
+    width.defaultNumberOfKids = 0;
 
-  var height = A2(Signal.lift, function(p){return p._1;}, dimensions);
-  height.defaultNumberOfKids = 0;
+    var height = A2(Signal.lift, function(p){return p._1;}, dimensions);
+    height.defaultNumberOfKids = 0;
 
-  function resizeIfNeeded() {
-      // Do not trigger event if the dimensions have not changed.
-      // This should be most of the time.
-      var w = getWidth();
-      var h = getHeight();
-      if (dimensions.value._0 === w && dimensions.value._1 === h) return;
+    function resizeIfNeeded() {
+        // Do not trigger event if the dimensions have not changed.
+        // This should be most of the time.
+        var w = getWidth();
+        var h = getHeight();
+        if (dimensions.value._0 === w && dimensions.value._1 === h) return;
 
-      setTimeout(function () {
-          // Check again to see if the dimensions have changed.
-          // It is conceivable that the dimensions have changed
-          // again while some other event was being processed.
-          var w = getWidth();
-          var h = getHeight();
-          if (dimensions.value._0 === w && dimensions.value._1 === h) return;
-          elm.notify(dimensions.id, Tuple2(w,h));
-      }, 0);
-  }
-  elm.addListener([dimensions.id], window, 'resize', resizeIfNeeded);
+        setTimeout(function () {
+            // Check again to see if the dimensions have changed.
+            // It is conceivable that the dimensions have changed
+            // again while some other event was being processed.
+            var w = getWidth();
+            var h = getHeight();
+            if (dimensions.value._0 === w && dimensions.value._1 === h) return;
+            localRuntime.notify(dimensions.id, Tuple2(w,h));
+        }, 0);
+    }
+    localRuntime.addListener([dimensions.id], window, 'resize', resizeIfNeeded);
 
-  return elm.Native.Window.values = {
-      dimensions:dimensions,
-      width:width,
-      height:height,
-      resizeIfNeeded:resizeIfNeeded
-  };
+    return localRuntime.Native.Window.values = {
+        dimensions: dimensions,
+        width: width,
+        height: height,
+        resizeIfNeeded: resizeIfNeeded
+    };
 
 };
