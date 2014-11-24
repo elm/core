92b4544dae5f5aa9028493d86dbf46ad4122d12d
jcollard <josephmcollard@gmail.com>
2014-11-24 13:46:10 -0500

    Added proposed implementation of since.

--- src/Time.elm
+++ src/Time.elm
@@ -79,7 +79,11 @@ Mouse.clicks)`` would result in a signal that is true for one second after
 each mouse click and false otherwise.
 -}
 since : Time -> Signal a -> Signal Bool
-since = Native.Time.since
+since t s = 
+  let start = Signal.map (always 1) s
+      stop = Signal.map (always -1) (delay t s)
+      delaydiff = Signal.foldp (+) 0 (Signal.merge start stop)
+  in Signal.map ((/=) 0) delaydiff
 
 {-| Add a timestamp to any signal. Timestamps increase monotonically. When you
 create `(timestamp Mouse.x)`, an initial timestamp is produced. The timestamp
