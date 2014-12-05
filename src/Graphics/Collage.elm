79097a2273f166f55c76da690e2800985fe9a1a4
jcollard <josephmcollard@gmail.com>
2014-12-05 14:01:55 -0500

    Fixed URL reference to Color.

--- src/Graphics/Collage.elm
+++ src/Graphics/Collage.elm
@@ -129,7 +129,7 @@ tiled to fill the entire shape.
 textured : String -> Shape -> Form
 textured src shape = fill (Texture src) shape
 
-{-| Fill a shape with a [gradient](/library/Color.elm#linear). -}
+{-| Fill a shape with a [gradient](Color#linear). -}
 gradient : Gradient -> Shape -> Form
 gradient grad shape = fill (Grad grad) shape
 
