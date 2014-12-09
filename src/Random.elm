2bdb720f55a04f2dfe2d11b990df56edc8c448e7
Evan Czaplicki <info@elm-lang.org>
2014-12-08 23:06:41 -0800

    Hide implementation of Generator type
    Taking the advice of @jvoigtlaender to keep the abstraction strong and
    design a proper API for constructing custom Generators

--- src/Random.elm
+++ src/Random.elm
@@ -62,9 +62,6 @@ module. It has a period of roughly 2.30584e18.
 
 @docs maxInt, minInt
 
-# Creating Custom Generators
-
-@docs Generator
 -}
 
 import Basics (..)
@@ -82,7 +79,8 @@ sufficient randomness is not guaranteed.
     int minInt maxInt  -- an integer in the widest range feasible
 -}
 int : Int -> Int -> Generator Int
-int a b seed =
+int a b =
+  Generator <| \seed ->
     let (lo,hi) = if a < b then (a,b) else (b,a)
 
         k = hi - lo + 1
@@ -126,11 +124,12 @@ minInt = -2147483648
     -- generate probability seed1 ==> (0.04, seed2)
 -}
 float : Float -> Float -> Generator Float
-float a b seed =
+float a b =
+  Generator <| \seed ->
     let (lo, hi) = if a < b then (a,b) else (b,a)
 
         (number, seed') =
-            int minInt maxInt seed
+            generate (int minInt maxInt) seed
 
         negativeOneToOne =
             toFloat number / toFloat (maxInt - minInt)
@@ -152,7 +151,8 @@ wide and 200 pixels tall.
 
 -}
 pair : Generator a -> Generator b -> Generator (a,b)
-pair genLeft genRight seed =
+pair (Generator genLeft) (Generator genRight) =
+  Generator <| \seed ->
     let (left , seed' ) = genLeft seed
         (right, seed'') = genRight seed'
     in
@@ -172,11 +172,12 @@ pair genLeft genRight seed =
         list 10 (pair int int)
 -}
 list : Int -> Generator a -> Generator (List a)
-list n gen =
-    listHelp [] n gen
+list n (Generator generate) =
+  Generator <| \seed ->
+    listHelp [] n generate seed
 
 
-listHelp : List a -> Int -> Generator a -> Generator (List a)
+listHelp : List a -> Int -> (Seed -> (a,Seed)) -> Seed -> (List a, Seed)
 listHelp list n generate seed =
     if n < 1
     then (List.reverse list, seed)
@@ -184,20 +185,12 @@ listHelp list n generate seed =
         let (value, seed') = generate seed
         in  listHelp (value :: list) (n-1) generate seed'
 
-{-| A `Generator` is a function that takes a seed, and then returns a random
-value and a new seed. The new seed is used to generate new random values. You
-can use this to define Generators of your own. For example, here is how
-`pair` is implemented.
 
-    pair : Generator a -> Generator b -> Generator (a,b)
-    pair genLeft genRight seed =
-        let (left , seed' ) = genLeft seed
-            (right, seed'') = genRight seed'
-        in
-            ((left,right), seed'')
+{-| A `Generator` is a function that takes a seed, and then returns a random
+value and a new seed. The new seed is used to generate new random values.
 -}
-type alias Generator a =
-    Seed -> (a, Seed)
+type Generator a =
+    Generator (Seed -> (a, Seed))
 
 type State = State Int Int
 
@@ -225,7 +218,7 @@ the same seed, you get the same results.
     -- generate (int 0 100) seed0 ==> (42, seed1)
 -}
 generate : Generator a -> Seed -> (a, Seed)
-generate generator seed =
+generate (Generator generator) seed =
     generator seed
 
 
