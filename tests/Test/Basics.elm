11b7f0ab3d867f8e47226587fa14fac9fea8bbfa
Alex Neslusan <deadfoxygrandpa@gmail.com>
2015-01-10 00:45:14 +0800

    Add tests for every remaining function in Basics

--- tests/Test/Basics.elm
+++ tests/Test/Basics.elm
@@ -13,6 +13,21 @@ tests =
             , test "clamp low" <| assertEqual 10 (clamp 10 20 5)
             , test "clamp mid" <| assertEqual 15 (clamp 10 20 15)
             , test "clamp high" <| assertEqual 20 (clamp 10 20 25)
+            , test "5 < 6" <| assertEqual True (5 < 6)
+            , test "6 < 5" <| assertEqual False (6 < 5)
+            , test "6 < 6" <| assertEqual False (6 < 6)
+            , test "5 > 6" <| assertEqual False (5 > 6)
+            , test "6 > 5" <| assertEqual True (6 > 5)
+            , test "6 > 6" <| assertEqual False (6 > 6)
+            , test "5 <= 6" <| assertEqual True (5 <= 6)
+            , test "6 <= 5" <| assertEqual False (6 <= 5)
+            , test "6 <= 6" <| assertEqual True (6 <= 6)
+            , test "compare \"A\" \"B\"" <| assertEqual LT (compare "A" "B")
+            , test "compare 'f' 'f'" <| assertEqual EQ (compare 'f' 'f')
+            , test "compare (1, 2, 3, 4, 5, 6) (0, 1, 2, 3, 4, 5)" <| assertEqual GT (compare (1, 2, 3, 4, 5, 6) (0, 1, 2, 3, 4, 5))
+            , test "compare ['a'] ['b']" <| assertEqual LT (compare ['a'] ['b'])
+            , test "==" <| assertEqual True (sqrt == sqrt)
+            , test "/=" <| assertEqual False (sqrt /= sqrt)
             ]
         toStringTests = suite "toString Tests"
             [ test "toString Int" <| assertEqual "42" (toString 42)
@@ -23,7 +38,7 @@ tests =
             , test "toString String single quote" <| assertEqual "\"not 'escaped'\"" (toString "not 'escaped'")
             , test "toString String double quote" <| assertEqual "\"are \\\"escaped\\\"\"" (toString "are \"escaped\"")
             ]
-        geometryTests = suite "Geometry Tests"
+        trigTests = suite "Trigonometry Tests"
             [ test "radians 0" <| assertEqual 0 (radians 0)
             , test "radians positive" <| assertEqual 5 (radians 5)
             , test "radians negative" <| assertEqual -5 (radians -5)
@@ -55,7 +70,7 @@ tests =
             , test "add int" <| assertEqual 17 ((round 10) + (round 7))
             , test "subtract float" <| assertEqual -6.3 (1 - 7.3)
             , test "subtract int" <| assertEqual 1130 ((round 9432) - (round 8302))
-            , test "multiply float" <| assertEqual 435.15 (96.7 * 4.5)
+            , test "multiply float" <| assertEqual 432 (96 * 4.5)
             , test "multiply int" <| assertEqual 90 ((round 10) * (round 9))
             , test "divide float" <| assertEqual 13.175 (527 / 40)
             , test "divide int" <| assertEqual 23 (70 // 3)
@@ -74,10 +89,64 @@ tests =
             , test "logBase 2 256" <| assertEqual 8 (logBase 2 256)
             , test "e" <| assert (abs (2.72 - e) < 0.01)
             ]
+        booleanTests = suite "Boolean Tests"
+            [ test "False && False" <| assertEqual False (False && False)
+            , test "False && True" <| assertEqual False (False && True)
+            , test "True && False" <| assertEqual False (True && False)
+            , test "True && True" <| assertEqual True (True && True)
+            , test "False || False" <| assertEqual False (False || False)
+            , test "False || True" <| assertEqual True (False || True)
+            , test "True || False" <| assertEqual True (True || False)
+            , test "True || True" <| assertEqual True (True || True)
+            , test "xor False False" <| assertEqual False (xor False False)
+            , test "xor False True" <| assertEqual True (xor False True)
+            , test "xor True False" <| assertEqual True (xor True False)
+            , test "xor True True" <| assertEqual False (xor True True)
+            , test "not True" <| assertEqual False (not True)
+            , test "not False" <| assertEqual True (not False)
+            , test "otherwise" <| assertEqual True otherwise
+            ]
+        conversionTests = suite "Conversion Tests"
+            [ test "round 0.6" <| assertEqual 1 (round 0.6)
+            , test "round 0.4" <| assertEqual 0 (round 0.4)
+            , test "round 0.5" <| assertEqual 1 (round 0.5)
+            , test "truncate -2367.9267" <| assertEqual -2367 (truncate -2367.9267)
+            , test "floor -2367.9267" <| assertEqual -2368 (floor -2367.9267)
+            , test "ceiling 37.2" <| assertEqual 38 (ceiling 37.2)
+            , test "toFloat 25" <| assertEqual 25 (toFloat 25)
+            ]
+        miscTests = suite "Miscellaneous Tests"
+            [ test "isNaN (0/0)" <| assertEqual True (isNaN (0/0))
+            , test "isNaN (sqrt -1)" <| assertEqual True (isNaN (sqrt -1))
+            , test "isNaN (1/0)" <| assertEqual False (isNaN (1/0))
+            , test "isNaN 1" <| assertEqual False (isNaN 1)
+            , test "isInfinite (0/0)" <| assertEqual False (isInfinite (0/0))
+            , test "isInfinite (sqrt -1)" <| assertEqual False (isInfinite (sqrt -1))
+            , test "isInfinite (1/0)" <| assertEqual True (isInfinite (1/0))
+            , test "isInfinite 1" <| assertEqual False (isInfinite 1)
+            , test "\"hello\" ++ \"world\"" <| assertEqual "helloworld" ("hello" ++ "world")
+            , test "[1, 1, 2] ++ [3, 5, 8]" <| assertEqual [1, 1, 2, 3, 5, 8] ([1, 1, 2] ++ [3, 5, 8])
+            , test "fst (1, 2)" <| assertEqual 1 (fst (1, 2))
+            , test "snd (1, 2)" <| assertEqual 2 (snd (1, 2))
+            ]
+        higherOrderTests = suite "Higher Order Helpers Tests"
+            [ test "identity 'c'" <| assertEqual 'c' (identity 'c')
+            , test "always 42 ()" <| assertEqual 42 (always 42 ())
+            , test "<|" <| assertEqual 9 (identity <| 3 + 6)
+            , test "|>" <| assertEqual 9 (3 + 6 |> identity)
+            , test "<<" <| assertEqual True (not << xor True <| True)
+            , test ">>" <| assertEqual True (True |> xor True >> not)
+            , test "flip" <| assertEqual 10 ((flip (//)) 2 20)
+            , test "curry" <| assertEqual 1 ((curry (\(a, b) -> a + b)) -5 6)
+            , test "uncurry" <| assertEqual 1 ((uncurry (+)) (-5, 6))
+            ]
     in
         suite "Basics"
             [ comparison
             , toStringTests
-            , geometryTests
+            , trigTests
             , basicMathTests
+            , booleanTests
+            , miscTests
+            , higherOrderTests
             ]
