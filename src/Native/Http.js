7239d91599f679a5169b09f6d7169509bd4dbaa0
jvoigtlaender <jvoigtlaender@users.noreply.github.com>
2015-01-22 07:48:18 +0100

    elm -> localRuntime in Http.js

--- src/Native/Http.js
+++ src/Native/Http.js
@@ -1,25 +1,31 @@
 Elm.Native.Http = {};
-Elm.Native.Http.make = function(elm) {
+Elm.Native.Http.make = function(localRuntime) {
 
-    elm.Native = elm.Native || {};
-    elm.Native.Http = elm.Native.Http || {};
-    if (elm.Native.Http.values) return elm.Native.Http.values;
+    localRuntime.Native = localRuntime.Native || {};
+    localRuntime.Native.Http = localRuntime.Native.Http || {};
+    if (localRuntime.Native.Http.values)
+    {
+        return localRuntime.Native.Http.values;
+    }
 
-    var List = Elm.List.make(elm);
-    var Signal = Elm.Signal.make(elm);
+    var List = Elm.List.make(localRuntime);
+    var Signal = Elm.Signal.make(localRuntime);
 
     function registerReq(queue,responses) {
         return function(req) {
-            if (req.url.length > 0) {
+            if (req.url.length > 0)
+            {
                 sendReq(queue,responses,req);
             }
         };
     }
 
     function updateQueue(queue,responses) {
-        if (queue.length > 0) {
-            elm.notify(responses.id, queue[0].value);
-            if (queue[0].value.ctor !== 'Waiting') {
+        if (queue.length > 0)
+        {
+            localRuntime.notify(responses.id, queue[0].value);
+            if (queue[0].value.ctor !== 'Waiting')
+            {
                 queue.shift();
                 setTimeout(function() { updateQueue(queue,responses); }, 0);
             }
@@ -35,7 +41,8 @@ Elm.Native.Http.make = function(elm) {
                        : new XMLHttpRequest());
 
         request.onreadystatechange = function(e) {
-            if (request.readyState === 4) {
+            if (request.readyState === 4)
+            {
                 response.value = (request.status >= 200 && request.status < 300 ?
                                   { ctor:'Success', _0:request.responseText } :
                                   { ctor:'Failure', _0:request.status, _1:request.statusText });
@@ -51,13 +58,13 @@ Elm.Native.Http.make = function(elm) {
     }
 
     function send(requests) {
-        var responses = Signal.constant(elm.Http.values.Waiting);
+        var responses = Signal.constant(localRuntime.Http.values.Waiting);
         var sender = A2( Signal.map, registerReq([],responses), requests );
         function f(x) { return function(y) { return x; } }
         return A3( Signal.map2, f, responses, sender );
     }
 
-    return elm.Native.Http.values = {
+    return localRuntime.Native.Http.values = {
         send:send
     };
 };
