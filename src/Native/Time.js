e495feb9bc9b797a31d8f93d7aa41cddd7115281
jvoigtlaender <jvoigtlaender@users.noreply.github.com>
2015-01-26 16:09:43 +0100

    put all state in signal in fpsWhen implementation

--- src/Native/Time.js
+++ src/Native/Time.js
@@ -27,13 +27,25 @@ Elm.Native.Time.make = function(localRuntime) {
         // Its value is a tuple with the current timestamp, and the state of isOn
         var input = NS.timestamp(A3(Signal.map2, F2(firstArg), Signal.dropRepeats(isOn), ticker));
 
-        var wasOn = false;
-        var timeoutID;
-        var previousTime = localRuntime.timer.programStart;
+        var timeoutID = 0;
+        if (isOn.value)
+        {
+            timeoutID = localRuntime.setTimeout(notifyTicker, msPerFrame);
+        }
 
-        function update(obj) {
+        var initialState = {
+            wasOn: isOn.value,
+            timeoutID: timeoutID,
+            previousTime: localRuntime.timer.programStart,
+            delta: 0
+        };
+
+        function update(obj,state) {
             var timestamp = obj._0;
             var isOn = obj._1;
+            var wasOn = state.wasOn;
+            var timeoutID = state.timeoutID;
+            var previousTime = state.previousTime;
 
             if (isOn)
             {
@@ -44,13 +56,16 @@ Elm.Native.Time.make = function(localRuntime) {
                 clearTimeout(timeoutID);
             }
 
-            var delta = (isOn && !wasOn) ? 0 : timestamp - previousTime;
-            wasOn = isOn;
-            previousTime = timestamp;
-            return delta;
+            return {
+                wasOn: isOn,
+                timeoutID: timeoutID,
+                previousTime: timestamp,
+                delta: (isOn && !wasOn) ? 0 : timestamp - previousTime
+            };
         }
 
-        return A2(Signal.map, update, input);
+        return A2( Signal.map, function(state) { return state.delta; },
+                   A3(Signal.foldp, F2(update), initialState, input) );
     }
 
 
