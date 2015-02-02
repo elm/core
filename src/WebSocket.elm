9abc67552c06e7e08c99826d4dc12ff9c29a2a3c
Evan Czaplicki <info@elm-lang.org>
2015-02-02 14:39:32 +0100

    Try to simplify the WebSockets API
    In the spirit of @Dandandan’s suggestion to be more promise-y. This API
    is a bit odd in that messages queue up even when no one is reading.

--- src/WebSocket.elm
+++ src/WebSocket.elm
@@ -1,5 +1,4 @@
 module WebSocket where
-
 {-| A library for low latency HTTP communication. See the HTTP library for
 standard requests like GET, POST, etc. The API of this library is likely to
 change to make it more flexible.
@@ -9,30 +8,24 @@ import Native.WebSocket
 import JavaScript.Decode as JavaScript
 
 
-type alias Config =
-    { url : String
-    , protocols : List String
-    }
-
-
-connect : Config -> Promise Error Connection
+-- url and protocols
+connect : String -> List String -> Promise x Connection
 connect =
   Native.WebSocket.connect
 
 
-send : Connection -> String -> Promise Error ()
-send =
-  Native.WebSocket.send
+type alias Closed =
+    { code : Int, reason : String, wasClean : Bool }
 
 
-type Event
-    = Message JavaScript.Value
-    | Close { code : Int, reason : String, wasClean : Bool }
+write : Connection -> String -> Promise Closed ()
+write =
+  Native.WebSocket.write
 
 
-listen : Connection -> (Event -> Promise x a) -> Promise x a
-listen =
-  Native.WebSocket.listen
+read : Connection -> Promise Closed JavaScript.Value
+read =
+  Native.WebSocket.read
 
 
 close : Connection -> Promise x ()
@@ -40,6 +33,6 @@ close =
   Native.WebSocket.close
 
 
-closeWith : Int -> String -> Connection -> Promise x ()
+closeWith : { code : Int, reason : String } -> Connection -> Promise x ()
 closeWith =
   Native.WebSocket.closeWith
