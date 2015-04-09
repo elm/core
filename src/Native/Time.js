9d52af2e1c651be2474d6df997ad7ecd6765058d
Evan Czaplicki <info@elm-lang.org>
2015-04-09 14:09:53 -0700

    Revert implementation of fpsWhen to 0.14 version
    cc @jvoigtlaender and @jwmerrill in case I messed something up!

--- src/Native/Time.js
+++ src/Native/Time.js
@@ -1,5 +1,6 @@
 Elm.Native.Time = {};
-Elm.Native.Time.make = function(localRuntime) {
+Elm.Native.Time.make = function(localRuntime)
+{
 
 	localRuntime.Native = localRuntime.Native || {};
 	localRuntime.Native.Time = localRuntime.Native.Time || {};
@@ -8,8 +9,10 @@ Elm.Native.Time.make = function(localRuntime) {
 		return localRuntime.Native.Time.values;
 	}
 
+	var Signal = Elm.Signal.make(localRuntime);
 	var NS = Elm.Native.Signal.make(localRuntime);
 	var Maybe = Elm.Maybe.make(localRuntime);
+	var Utils = Elm.Native.Utils.make(localRuntime);
 
 
 	// FRAMES PER SECOND
@@ -17,87 +20,64 @@ Elm.Native.Time.make = function(localRuntime) {
 	function fpsWhen(desiredFPS, isOn)
 	{
 		var msPerFrame = 1000 / desiredFPS;
-		var ticker = NS.input('fpsWhen', null);
+		var ticker = NS.input('fps-' + desiredFPS, Utils.Tuple0);
 
 		function notifyTicker()
 		{
-			localRuntime.notify(ticker.id, null);
+			localRuntime.notify(ticker.id, Utils.Tuple0);
 		}
 
-		var onOffEvents = A2(NS.streamMap, onOff, isOn);
-		var events = A3(NS.genericMerge, F2(firstArg), onOffEvents, ticker);
-		var timestampedEvents = NS.timestamp(events);
+		function firstArg(x, y)
+		{
+			return x;
+		}
 
-		var emptyState = {
-			isOn: isOn.initialValue,
+		// input fires either when isOn changes, or when ticker fires.
+		// Its value is a tuple with the current timestamp, and the state of isOn
+		var input = NS.timestamp(A3(Signal.map2, F2(firstArg), Signal.dropRepeats(isOn), ticker));
+
+		var initialState = {
+			isOn: false,
 			time: localRuntime.timer.programStart,
-			delta: null
+			delta: 0
 		};
 
-		if (emptyState.isOn)
+		var timeoutId;
+
+		function update(input,state)
 		{
-			localRuntime.setTimeout(notifyTicker, 0);
-		}
+			var currentTime = input._0;
+			var isOn = input._1;
+			var wasOn = state.isOn;
+			var previousTime = state.time;
 
-		var timeoutId = 0;
-
-		function fpsUpdate(rawEvent, state)
-		{
-			var currentTime = rawEvent._0;
-			var event = rawEvent._1;
-
-			if (event === null)
+			if (isOn)
 			{
 				timeoutId = localRuntime.setTimeout(notifyTicker, msPerFrame);
-				return {
-					isOn: state.isOn,
-					time: currentTime,
-					delta: currentTime - state.time
-				};
 			}
-			else if (!state.isOn && event)
-			{
-				timeoutId = localRuntime.setTimeout(notifyTicker, msPerFrame);
-				return {
-					isOn: true,
-					time: currentTime,
-					delta: null
-				};
-			}
-			else if (state.isOn && !event)
+			else if (wasOn)
 			{
 				clearTimeout(timeoutId);
-				return {
-					isOn: false,
-					time: currentTime,
-					delta: null
-				};
 			}
+
+			return {
+				isOn: isOn,
+				time: currentTime,
+				delta: (isOn && !wasOn) ? 0 : currentTime - previousTime
+			};
 		}
-		var state = A3(NS.fold, F2(fpsUpdate), emptyState, timestampedEvents);
-		return A2(NS.filterMap, toDelta, state);
+
+		return A2(
+			Signal.map,
+			function(state) { return state.delta; },
+			A3(Signal.foldp, F2(update), update(input.value,initialState), input)
+		);
 	}
 
-	function onOff(isOn)
-	{
-		return {
-			ctor: 'OnOff',
-			_0: isOn
-		};
-	}
 
-	function toDelta(state)
+	function fps(t)
 	{
-		if (state.isOn && state.delta !== null)
-		{
-			return Maybe.Just(state.delta);
-		}
-		return Maybe.Nothing;
-	}
-
-	function firstArg(x,y)
-	{
-		return x;
+		return fpsWhen(t, Signal.constant(true));
 	}
 
 
@@ -105,12 +85,12 @@ Elm.Native.Time.make = function(localRuntime) {
 
 	function every(t)
 	{
-		var ticker = NS.input('every', null);
+		var ticker = NS.input('every-' + t, Utils.Tuple0);
 		function tellTime()
 		{
-			localRuntime.notify(ticker.id, null);
+			localRuntime.notify(ticker.id, Utils.Tuple0);
 		}
-		var clock = A2( NS.map, fst, NS.timestamp(ticker) );
+		var clock = A2( Signal.map, fst, NS.timestamp(ticker) );
 		setInterval(tellTime, t);
 		return clock;
 	}
@@ -130,6 +110,7 @@ Elm.Native.Time.make = function(localRuntime) {
 
 	return localRuntime.Native.Time.values = {
 		fpsWhen: F2(fpsWhen),
+		fps: fps,
 		every: every,
 		toDate: function(t) { return new window.Date(t); },
 		read: read
