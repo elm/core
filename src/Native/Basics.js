62765f64447125cd243d2bcf1724341c7a0e77b6
Alexander Thiemann <mail@athiemann.net>
2015-10-03 16:07:55 +0200

    more spaces to tabs

--- src/Native/Basics.js
+++ src/Native/Basics.js
@@ -92,9 +92,9 @@ Elm.Native.Basics.make = function(localRuntime) {
 		var y = point._1;
 		return Utils.Tuple2(Math.sqrt(x * x + y * y), Math.atan2(y, x));
 	}
-    function toFixedRep(num, fix) {
-        return num.toFixed(fix);
-    }
+	function toFixedRep(num, fix) {
+		return num.toFixed(fix);
+	}
 
 	return localRuntime.Native.Basics.values = {
 		div: F2(div),
