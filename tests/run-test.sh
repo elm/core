abce951daa6f015e61b67deb74725aed475bfd54
Jonas Coch <jonas@iosphere.de>
2015-11-23 10:44:54 +0100

    Update test script
    The generated JS file is now patched with the script
    included in elm-console.

--- tests/run-test.sh
+++ tests/run-test.sh
@@ -9,10 +9,19 @@ VERSION_DIR="$(ls elm-stuff/packages/elm-lang/core/)"
 CORE_PACKAGE_DIR="elm-stuff/packages/elm-lang/core/$VERSION_DIR"
 CORE_GIT_DIR="$(dirname $PWD)"
 
+ELM_CONSOLE_VERSION_DIR="$(ls elm-stuff/packages/laszlopandy/elm-console/)"
+ELM_CONSOLE_IO_SRIPT="elm-stuff/packages/laszlopandy/elm-console/$ELM_CONSOLE_VERSION_DIR/elm-io.sh"
+
 echo "Linking $CORE_PACKAGE_DIR to $CORE_GIT_DIR"
 rm -rf $CORE_PACKAGE_DIR
 ln -s $CORE_GIT_DIR $CORE_PACKAGE_DIR
 
-elm-make --yes --output test.js Test.elm
-cat elm-io-ports.js >> test.js
+echo "Compiling Test.elm to raw-test.js"
+elm-make --yes --output raw-test.js Test.elm
+
+echo "Patching raw-test.js to test.js"
+chmod +x $ELM_CONSOLE_IO_SRIPT
+./$ELM_CONSOLE_IO_SRIPT raw-test.js test.js
+
+echo "Running test.js"
 node test.js
