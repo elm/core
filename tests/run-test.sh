4e8bc32ae73149c3f2f35e322527bb0c547fad31
Alex Neslusan <deadfoxygrandpa@gmail.com>
2015-11-24 14:59:44 +0800

    Update test runner script for elm-console

--- tests/run-test.sh
+++ tests/run-test.sh
@@ -3,16 +3,17 @@
 cd "$(dirname "$0")"
 set -e
 
-elm-package install -y
 
 VERSION_DIR="$(ls elm-stuff/packages/elm-lang/core/)"
 CORE_PACKAGE_DIR="elm-stuff/packages/elm-lang/core/$VERSION_DIR"
 CORE_GIT_DIR="$(dirname $PWD)"
+ELM_CONSOLE_VERSION_DIR="$(ls elm-stuff/packages/laszlopandy/elm-console/)"
+ELM_IO_PATH="elm-stuff/packages/laszlopandy/elm-console/$ELM_CONSOLE_VERSION_DIR/elm-io.sh"
 
 echo "Linking $CORE_PACKAGE_DIR to $CORE_GIT_DIR"
 rm -rf $CORE_PACKAGE_DIR
 ln -s $CORE_GIT_DIR $CORE_PACKAGE_DIR
 
-elm-make --yes --output test.js Test.elm
-cat elm-io-ports.js >> test.js
+elm-make --yes --output raw-test.js Test.elm
+bash $ELM_IO_PATH raw-test.js test.js 
 node test.js
