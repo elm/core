2013740bfab041fd0f64bb8bb1d6c8249849c5b5
Evan Czaplicki <info@elm-lang.org>
2016-02-18 17:03:52 -0800

    Move Native.Bitwise functions into exports

--- src/Native/Bitwise.js
+++ src/Native/Bitwise.js
@@ -1,18 +1,10 @@
 
-function and(a, b) { return a & b; }
-function or(a, b) { return a | b; }
-function xor(a, b) { return a ^ b; }
-function not(a) { return ~a; }
-function sll(a, offset) { return a << offset; }
-function sra(a, offset) { return a >> offset; }
-function srl(a, offset) { return a >>> offset; }
-
 return {
-	and: F2(and),
-	or: F2(or),
-	xor: F2(xor),
-	complement: not,
-	shiftLeft: F2(sll),
-	shiftRightArithmatic: F2(sra),
-	shiftRightLogical: F2(srl)
+	and: F2(function and(a, b) { return a & b; }),
+	or: F2(function or(a, b) { return a | b; }),
+	xor: F2(function xor(a, b) { return a ^ b; }),
+	complement: function complement(a) { return ~a; },
+	shiftLeft: F2(function sll(a, offset) { return a << offset; }),
+	shiftRightArithmatic: F2(function sra(a, offset) { return a >> offset; }),
+	shiftRightLogical: F2(function srl(a, offset) { return a >>> offset; })
 };
