82c8e4e73f58071f41fb10ac772ab7277042f660
Evan Czaplicki <info@elm-lang.org>
2016-02-23 18:53:59 -0500

    Point users to Result.withDefault directly from String.toInt docs
    Thanks again to @christinecha for revealing that the typical learning
    path will probably bring you to results through text input.

--- src/String.elm
+++ src/String.elm
@@ -409,10 +409,16 @@ indices =
 
 {-| Try to convert a string into an int, failing on improperly formatted strings.
 
-    toInt "123" == Ok 123
-    toInt "-42" == Ok -42
-    toInt "3.1" == Err "could not convert string '3.1' to an Int"
-    toInt "31a" == Err "could not convert string '31a' to an Int"
+    String.toInt "123" == Ok 123
+    String.toInt "-42" == Ok -42
+    String.toInt "3.1" == Err "could not convert string '3.1' to an Int"
+    String.toInt "31a" == Err "could not convert string '31a' to an Int"
+
+If you are extracting a number from some raw user input, you will typically
+want to use [`Result.withDefault`](Result#withDefault) to handle bad data:
+
+    Result.withDefault 0 (String.toInt "42") == 42
+    Result.withDefault 0 (String.toInt "ab") == 0
 -}
 toInt : String -> Result String Int
 toInt =
@@ -421,10 +427,16 @@ toInt =
 
 {-| Try to convert a string into a float, failing on improperly formatted strings.
 
-    toFloat "123" == Ok 123.0
-    toFloat "-42" == Ok -42.0
-    toFloat "3.1" == Ok 3.1
-    toFloat "31a" == Err "could not convert string '31a' to a Float"
+    String.toFloat "123" == Ok 123.0
+    String.toFloat "-42" == Ok -42.0
+    String.toFloat "3.1" == Ok 3.1
+    String.toFloat "31a" == Err "could not convert string '31a' to a Float"
+
+If you are extracting a number from some raw user input, you will typically
+want to use [`Result.withDefault`](Result#withDefault) to handle bad data:
+
+    Result.withDefault 0 (String.toFloat "42.5") == 42.5
+    Result.withDefault 0 (String.toFloat "cats") == 0
 -}
 toFloat : String -> Result String Float
 toFloat =
