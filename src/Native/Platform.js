44b6f11cc7d2c33e2a030755856f4dc5c305a818
Evan Czaplicki <info@elm-lang.org>
2016-03-01 14:44:40 -0800

    Reorder functions in Native.Platform

--- src/Native/Platform.js
+++ src/Native/Platform.js
@@ -3,6 +3,61 @@
 var _elm_lang$core$Native_Platform = function() {
 
 
+// PROGRAMS
+
+
+function addPublicModule(object, name, main)
+{
+	object['fullscreen'] = main ? fullscreenFor(main) : mainIsUndefined(name);
+}
+
+function mainIsUndefined(name)
+{
+	return function() {
+		throw new Error(
+			'Cannot initialize module ' + name +
+			' because it has no `main` value! What would I run?'
+		);
+	};
+}
+
+function fullscreenFor(program)
+{
+	if (!program.renderer)
+	{
+		return function fullscreen()
+		{
+			_evancz$virtual_dom$Native_VirtualDom.staticProgram(document.body, program);
+		}
+	}
+
+	return function fullscreen(flags)
+	{
+		var update = program.update;
+		var subs = program.subscriptions;
+		var view = program.view;
+
+		var tuple = program.init(flags);
+		var model = tuple._0;
+		var initialCmds = categorizeEffects(tuple._1);
+		var initialSubs = categorizeEffects(subs(model));
+
+		var renderer = program.renderer(document.body, enqueue, view(model));
+
+		function enqueue(msg)
+		{
+			// TODO this may be mean user events can "cut" to the front
+			// of the event queue. If so, do it another way instead.
+			var tuple = A2(update, msg, model);
+			model = tuple._0;
+			categorizeEffects(tuple._1);
+			renderer.update(view(model));
+		}
+	};
+}
+
+
+
 // BAGS
 
 
@@ -83,57 +138,6 @@ function categorizeEffectsHelp(bag, effectDict)
 	}
 }
 
-
-
-// PROGRAMS
-
-
-function fullscreenFor(program)
-{
-	if (!program.renderer)
-	{
-		return function fullscreen()
-		{
-			_evancz$virtual_dom$Native_VirtualDom.staticProgram(document.body, program);
-		}
-	}
-
-	return function fullscreen(flags)
-	{
-		var update = program.update;
-		var subs = program.subscriptions;
-		var view = program.view;
-
-		var tuple = program.init(flags);
-		var model = tuple._0;
-		var initialCmds = categorizeEffects(tuple._1);
-		var initialSubs = categorizeEffects(subs(model));
-
-		var renderer = program.renderer(document.body, enqueue, view(model));
-
-		function enqueue(msg)
-		{
-			// TODO this may be mean user events can "cut" to the front
-			// of the event queue. If so, do it another way instead.
-			var tuple = A2(update, msg, model);
-			model = tuple._0;
-			categorizeEffects(tuple._1);
-			renderer.update(view(model));
-		}
-	};
-}
-
-function mainIsUndefined(name)
-{
-	return function() {
-		throw new Error('Cannot initialize module ' + name + ' because it has no `main` value! What would I run?')
-	};
-}
-
-function addPublicModule(object, name, main)
-{
-	object['fullscreen'] = main ? fullscreenFor(main) : mainIsUndefined(name);
-}
 
 return {
 	addPublicModule: addPublicModule,
