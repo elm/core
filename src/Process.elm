c30db79804d47c3b6bf44ccf6fb4940656efdae6
Evan Czaplicki <info@elm-lang.org>
2016-03-07 16:46:44 -0800

    Simplify Process API

--- src/Process.elm
+++ src/Process.elm
@@ -1,15 +1,46 @@
 module Process
-  ( Process
+  ( Id
   , spawn
-  , send
   , kill
   )
   where
 {-|
 
 # Processes
-@docs Process, spawn, send, kill
+@docs Id, spawn, kill
 
+## Future Plans
+
+Right now, this library is pretty sparse. For example, there is no public API
+for processes to communicate with each other. This is a really important
+ability, but it is also something that is extraordinarily easy to get wrong!
+
+I think the trend will be towards an Erlang style of concurrency, where every
+process has an “event queue” that anyone can send messages to. I currently
+think the API will be extended to be more like this:
+
+    type Id exit msg
+
+    spawn : Task exit a -> Task x (Id exit Never)
+
+    kill : Id exit msg -> Task x ()
+
+    send : Id exit msg -> msg -> Task x ()
+
+A process `Id` will have two type variables to make sure all communication is
+valid. The `exit` type describes the messages that are produced if the process
+fails because of user code. So if processes are linked and trapping errors,
+they will need to handle this. The `msg` type just describes what kind of
+messages this process can be sent by strangers.
+
+We shall see though! This is just a draft that does not cover nearly everything
+it needs to, so the long-term vision for concurrency in Elm will be rolling out
+slowly as I get more data and experience.
+
+I ask that people bullish on compiling to node.js keep this in mind. I think we
+can do better than the hopelessly bad concurrency model of node.js, and I hope
+the Elm community will be supportive of being more ambitious, even if it takes
+longer. That’s kind of what Elm is all about.
 -}
 
 import Basics exposing (Never)
@@ -30,8 +61,8 @@ sequences at the exact same time. For example, a
 concurrent, but not necessarily parallel. So even though JS runs within a
 single OS-level thread, Elm can still run things concurrently.
 -}
-type alias Process exit msgs =
-  Platform.Process exit msgs
+type alias Id =
+  Platform.ProcessId
 
 
 {-| Run a task in its own light-weight process. In the following example,
@@ -45,7 +76,7 @@ there.
 cannot receive any messages. More flexibility for user-defined processes will
 come in a later release!
 -}
-spawn : Task x a -> Task y (Process x Never)
+spawn : Task x a -> Task y Id
 spawn =
   Native.Scheduler.spawn
 
@@ -55,19 +86,7 @@ have it keep running and doing stuff. The `kill` function will force a process
 to bail on whatever task it is running. So if there is an HTTP request in
 flight, it will also abort the request.
 -}
-kill : Process exit msg -> Task x ()
+kill : Id -> Task x ()
 kill =
   Native.Scheduler.kill
-
-
-{-| Send a message to a process's message queue.
-
-**Note:** Right now users can only `spawn` processes that never receive
-messages. If you are defining an `effect module` (which for 99% of readers
-should not be) you will need to use this function.
--}
-send : Process exit msg -> msg -> Task x ()
-send =
-  Native.Scheduler.send
-
 
