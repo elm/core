3e8296ca829464ee59bac6d66637513a067799c6
Evan Czaplicki <info@elm-lang.org>
2016-03-11 15:46:35 -0800

    Add Time.every

--- src/Native/Time.js
+++ src/Native/Time.js
@@ -2,12 +2,26 @@
 
 var _elm_lang$core$Native_Time = function() {
 
-var now = _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
+var now = _elm_lang$core$Native_Scheduler.nativeBinding(function(callback)
+{
 	callback(_elm_lang$core$Native_Scheduler.succeed(Date.now()));
 });
 
+function setInterval_(interval, task)
+{
+	return _elm_lang$core$Native_Scheduler.nativeBinding(function(callback)
+	{
+		var id = setInterval(function() {
+			_elm_lang$core$Native_Scheduler.rawSpawn(task);
+		}, interval);
+
+		return function() { clearInterval(id); };
+	});
+}
+
 return {
 	now: now,
+	setInterval_: F2(setInterval_)
 };
 
 }();
--- src/Time.elm
+++ src/Time.elm
@@ -1,14 +1,16 @@
-module Time
+effect module Time { subscription = MySub }
     ( Time
-    , now
+    , now, every
     , millisecond, second, minute, hour
     , inMilliseconds, inSeconds, inMinutes, inHours
-    ) where
+    )
+    where
+
 
 {-| Library for working with time.
 
 # Time
-@docs Time, now
+@docs Time, now, every
 
 # Units
 @docs millisecond, second, minute, hour,
@@ -16,8 +18,15 @@ module Time
 
 -}
 
+
 import Basics exposing (..)
+import Dict
+import List exposing ((::))
+import Maybe exposing (Maybe(..))
+import Native.Scheduler
 import Native.Time
+import Platform
+import Platform.Sub exposing (Sub)
 import Task exposing (Task)
 
 
@@ -36,6 +45,31 @@ type alias Time = Float
 now : Task x Time
 now =
   Native.Time.now
+
+
+{-| Subscribe to the current time. First you provide an interval describing how
+frequently you want updates. Second, you give a tagger that turns a time into a
+message for your `update` function. So if you want to hear about the current
+time every second, you would say something like this:
+
+    type Msg = Tick Time | ...
+
+    subscriptions model =
+      every second Tick
+
+Check out the [Elm Architecture Tutorial][arch] for more info on how
+subscriptions work.
+
+[arch]: https://github.com/evancz/elm-architecture-tutorial/
+
+**Note:** this function is not for animation! You need to use something based
+on `requestAnimationFrame` to get smooth animations. This is based on
+`setInterval` which is better for recurring tasks like “check on something
+every 30 seconds”.
+-}
+every : Time -> (Time -> msg) -> Sub msg
+every interval tagger =
+  subscription (Every interval tagger)
 
 
 
@@ -90,3 +124,116 @@ inMinutes t =
 inHours : Time -> Float
 inHours t =
   t / hour
+
+
+
+-- SUBSCRIPTIONS
+
+
+type MySub msg =
+  Every Time (Time -> msg)
+
+
+subMap : (a -> b) -> MySub a -> MySub b
+subMap f (Every interval tagger) =
+  Every interval (f << tagger)
+
+
+
+-- EFFECT MANAGER
+
+
+type alias State msg =
+  { taggers : Taggers msg
+  , processes : Processes
+  }
+
+
+type alias Processes =
+  Dict.Dict Time Platform.ProcessId
+
+
+type alias Taggers msg =
+  Dict.Dict Time (List (Time -> msg))
+
+
+init : Task Never (State msg)
+init =
+  Task.succeed (State Dict.empty Dict.empty)
+
+
+onEffects : Platform.Router msg Time -> List (MySub msg) -> State msg -> Task Never (State msg)
+onEffects router subs {processes} =
+  let
+    newTaggers =
+      List.foldl addMySub Dict.empty subs
+
+    leftStep interval taggers (spawnList, existingDict, killTask) =
+      (interval :: spawnList, existingDict, killTask)
+
+    bothStep interval taggers id (spawnList, existingDict, killTask) =
+      (spawnList, Dict.insert interval id existingDict, killTask)
+
+    rightStep _ id (spawnList, existingDict, killTask) =
+      (spawnList, existingDict, Native.Scheduler.kill id `Task.andThen` \_ -> killTask)
+
+    (spawnList, existingDict, killTask) =
+      Dict.merge
+        leftStep
+        bothStep
+        rightStep
+        newTaggers
+        processes
+        ([], Dict.empty, Task.succeed ())
+  in
+    killTask
+      `Task.andThen` \_ ->
+
+    spawnHelp router spawnList existingDict
+      `Task.andThen` \newProcesses ->
+
+    Task.succeed (State newTaggers newProcesses)
+
+
+addMySub : MySub msg -> Taggers msg -> Taggers msg
+addMySub (Every interval tagger) state =
+  case Dict.get interval state of
+    Nothing ->
+      Dict.insert interval [tagger] state
+
+    Just taggers ->
+      Dict.insert interval (tagger :: taggers) state
+
+
+spawnHelp : Platform.Router msg Time -> List Time -> Processes -> Task.Task x Processes
+spawnHelp router intervals processes =
+  case intervals of
+    [] ->
+      Task.succeed processes
+
+    interval :: rest ->
+      Native.Scheduler.spawn (setInterval interval (Platform.sendToSelf router interval))
+        `Task.andThen` \id ->
+
+      spawnHelp router rest (Dict.insert interval id processes)
+
+
+onSelfMsg : Platform.Router msg Time -> Time -> State msg -> Task Never (State msg)
+onSelfMsg router interval state =
+  case Dict.get interval state.taggers of
+    Nothing ->
+      Task.succeed state
+
+    Just taggers ->
+      now
+        `Task.andThen` \time ->
+
+      Task.sequence (List.map (\tagger -> Platform.sendToApp router (tagger time)) taggers)
+        `Task.andThen` \_ ->
+
+      Task.succeed state
+
+
+setInterval : Time -> Task Never () -> Task x Never
+setInterval =
+  Native.Time.setInterval_
