10938f2c7f5d6168b40b117007ec80aecd4192f2
Evan Czaplicki <evancz@users.noreply.github.com>
2016-07-12 23:01:50 -0700

    Provide flagDecoder before final setup of program
    Also check that flagDecoder is defined if the program was created with
    programWithFlags.

--- src/Native/Platform.js
+++ src/Native/Platform.js
@@ -7,52 +7,67 @@ var _elm_lang$core$Native_Platform = function() {
 
 function program(impl)
 {
-	return function(object, moduleName, flagDecoder)
+	return function(flagDecoder)
 	{
-		object['worker'] = function worker(flags)
+		return function(object, moduleName)
 		{
-			if (typeof flags !== 'undefined')
+			object['worker'] = function worker(flags)
 			{
-				throw new Error(
-					'The `' + moduleName + '` module does not need flags.\n'
-					'Call ' + moduleName + '.worker() with no arguments and you should be all set!'
-				);
-			}
+				if (typeof flags !== 'undefined')
+				{
+					throw new Error(
+						'The `' + moduleName + '` module does not need flags.\n'
+						'Call ' + moduleName + '.worker() with no arguments and you should be all set!'
+					);
+				}
 
-			return initialize(
-				impl.init,
-				impl.update,
-				impl.subscriptions,
-				function view(model) {},
-				renderer
-			);
+				return initialize(
+					impl.init,
+					impl.update,
+					impl.subscriptions,
+					function view(model) {},
+					renderer
+				);
+			};
 		};
 	};
 }
 
 function programWithFlags(impl)
 {
-	return function(object, moduleName, flagDecoder)
+	return function(flagDecoder)
 	{
-		object['worker'] = function worker(flags)
+		return function(object, moduleName)
 		{
-			var result = A2(_elm_lang$core$Native_Json.run, flagDecoder, flags);
-			if (result.ctor === 'Err')
+			object['worker'] = function worker(flags)
 			{
-				throw new Error(
-					moduleName + '.worker(...) was called with an unexpected argument.\n'
-					+ 'I tried to convert it to an Elm value, but ran into this problem:\n\n'
-					+ result._0
-				);
-			}
+				if (typeof flagDecoder === 'undefined')
+				{
+					throw new Error(
+						'Are you trying to sneak a Never value into Elm? Trickster!\n'
+						+ 'It looks like ' + moduleName + '.main is defined with `programWithFlags` but has type `Program Never`.\n'
+						+ 'Use `program` instead if you do not want flags.'
+					);
+				}
 
-			return initialize(
-				impl.init(result._0),
-				impl.update,
-				impl.subscriptions,
-				function view(model) {},
-				renderer
-			);
+				var result = A2(_elm_lang$core$Native_Json.run, flagDecoder, flags);
+				if (result.ctor === 'Err')
+				{
+					throw new Error(
+						moduleName + '.worker(...) was called with an unexpected argument.\n'
+						+ 'I tried to convert it to an Elm value, but ran into this problem:\n\n'
+						+ result._0
+					);
+				}
+
+				return initialize(
+					impl.init(result._0),
+					impl.update,
+					impl.subscriptions,
+					function view(model) {},
+					renderer
+				);
+			};
 		};
 	};
 }
