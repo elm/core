7ed9c5e0cb1f733316ee1482d34777069d8014c7
Martin Janiczek <martin@janiczek.cz>
2016-08-09 22:42:22 +0200

    Support higher Unicode ranges for Char.fromCode

--- src/Native/Char.js
+++ src/Native/Char.js
@@ -3,7 +3,7 @@
 var _elm_lang$core$Native_Char = function() {
 
 return {
-	fromCode: function(c) { return _elm_lang$core$Native_Utils.chr(String.fromCharCode(c)); },
+	fromCode: function(c) { return _elm_lang$core$Native_Utils.chr(String.fromCodePoint(c)); },
 	toCode: function(c) { return c.charCodeAt(0); },
 	toUpper: function(c) { return _elm_lang$core$Native_Utils.chr(c.toUpperCase()); },
 	toLower: function(c) { return _elm_lang$core$Native_Utils.chr(c.toLowerCase()); },
