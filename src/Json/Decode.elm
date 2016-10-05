9c5087b95c1bd48e3210d7469d70a8d7f0738913
Evan Czaplicki <evancz@users.noreply.github.com>
2016-10-05 11:49:09 -0700

    Rename (:=) to Json.Decode.field
    Following the API design guidelines finally.
    http://package.elm-lang.org/help/design-guidelines#avoid-infix-operators

--- src/Json/Decode.elm
+++ src/Json/Decode.elm
@@ -3,7 +3,7 @@ module Json.Decode exposing
   , decodeString, decodeValue
   , string, int, float, bool, null
   , list, array
-  , (:=), at
+  , field, at
   , object1, object2, object3, object4, object5, object6, object7, object8
   , keyValuePairs, dict
   , maybe, oneOf, map, fail, succeed, andThen
@@ -42,11 +42,11 @@ decodeString =
 
 at : List String -> Decoder a -> Decoder a
 at fields decoder =
-    List.foldr (:=) decoder fields
+    List.foldr field decoder fields
 
 
-(:=) : String -> Decoder a -> Decoder a
-(:=) =
+field : String -> Decoder a -> Decoder a
+field =
     Native.Json.decodeField
 
 
