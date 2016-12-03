3e7023cf3d0887262c500a7b0108c1cc930c5748
Marek Fajkus <marek.faj@gmail.com>
2016-12-03 23:05:37 +0100

    upgrade Json

--- tests/Test/Json.elm
+++ tests/Test/Json.elm
@@ -2,15 +2,15 @@ module Test.Json exposing (tests)
 
 import Basics exposing (..)
 import Result exposing (..)
-import Json.Decode as Json exposing ((:=))
+import Json.Decode as Json
 import String
 
-import ElmTest exposing (..)
-
+import Test exposing (..)
+import Expect
 
 tests : Test
 tests =
-  suite "Json decode"
+  describe "Json decode"
     [ intTests
     , customTests
     ]
@@ -22,32 +22,32 @@ intTests =
     testInt val str =
       case Json.decodeString Json.int str of
         Ok _ ->
-          assertEqual val True
+          Expect.equal val True
 
         Err _ ->
-          assertEqual val False
+          Expect.equal val False
   in
-    suite "Json decode int"
-      [ test "whole int" <| testInt True "4"
-      , test "-whole int" <| testInt True "-4"
-      , test "whole float" <| testInt True "4.0"
-      , test "-whole float" <| testInt True "-4.0"
-      , test "large int" <| testInt True "1801439850948"
-      , test "-large int" <| testInt True "-1801439850948"
-      , test "float" <| testInt False "4.2"
-      , test "-float" <| testInt False "-4.2"
-      , test "Infinity" <| testInt False "Infinity"
-      , test "-Infinity" <| testInt False "-Infinity"
-      , test "NaN" <| testInt False "NaN"
-      , test "-NaN" <| testInt False "-NaN"
-      , test "true" <| testInt False "true"
-      , test "false" <| testInt False "false"
-      , test "string" <| testInt False "\"string\""
-      , test "object" <| testInt False "{}"
-      , test "null" <| testInt False "null"
-      , test "undefined" <| testInt False "undefined"
-      , test "Decoder expects object finds array, was crashing runtime." <|
-          assertEqual
+    describe "Json decode int"
+      [ test "whole int" <| \() -> testInt True "4"
+      , test "-whole int" <| \() -> testInt True "-4"
+      , test "whole float" <| \() -> testInt True "4.0"
+      , test "-whole float" <| \() -> testInt True "-4.0"
+      , test "large int" <| \() -> testInt True "1801439850948"
+      , test "-large int" <| \() -> testInt True "-1801439850948"
+      , test "float" <| \() -> testInt False "4.2"
+      , test "-float" <| \() -> testInt False "-4.2"
+      , test "Infinity" <| \() -> testInt False "Infinity"
+      , test "-Infinity" <| \() -> testInt False "-Infinity"
+      , test "NaN" <| \() -> testInt False "NaN"
+      , test "-NaN" <| \() -> testInt False "-NaN"
+      , test "true" <| \() -> testInt False "true"
+      , test "false" <| \() -> testInt False "false"
+      , test "string" <| \() -> testInt False "\"string\""
+      , test "object" <| \() -> testInt False "{}"
+      , test "null" <| \() -> testInt False "null"
+      , test "undefined" <| \() -> testInt False "undefined"
+      , test "Decoder expects object finds array, was crashing runtime." <| \() ->
+          Expect.equal
             (Err "Expecting an object but instead got: []")
             (Json.decodeString (Json.dict Json.float) "[]")
       ]
@@ -63,21 +63,21 @@ customTests =
       "I want to see this message!"
 
     myDecoder =
-      Json.customDecoder ("foo" := Json.string) (\_ -> Err customErrorMessage)
+      Json.field "foo" Json.string |> Json.andThen (\_ -> Json.fail customErrorMessage)
 
     assertion  =
       case Json.decodeString myDecoder jsonString of
         Ok _ ->
-          fail "expected `customDecoder` to produce a value of type Err, but got Ok"
+          Expect.fail "expected `customDecoder` to produce a value of type Err, but got Ok"
 
         Err message ->
           if String.contains customErrorMessage message then
-            pass
+            Expect.pass
 
           else
-            fail <|
+            Expect.fail <|
               "expected `customDecoder` to preserve user's error message '"
               ++ customErrorMessage ++ "', but instead got: " ++ message
   in
-    test "customDecoder preserves user error messages" assertion
+    test "customDecoder preserves user error messages" <| \() -> assertion
 
