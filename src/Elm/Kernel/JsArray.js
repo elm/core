64a847eb31d5739066a5ec4c84810fcf739ef4f7
Evan Czaplicki <evancz@users.noreply.github.com>
2018-01-16 15:27:09 -0800

    Use better variable names in JsArray.js
    arr => array
    idx => index

--- src/Elm/Kernel/JsArray.js
+++ src/Elm/Kernel/JsArray.js
@@ -7,22 +7,23 @@ import Elm.Kernel.Utils exposing (Tuple2)
 
 var _JsArray_empty = [];
 
-function _JsArray_singleton(val)
+function _JsArray_singleton(value)
 {
-    return [val];
+    return [value];
 }
 
-function _JsArray_length(arr)
+function _JsArray_length(array)
 {
-    return arr.length;
+    return array.length;
 }
 
-var _JsArray_initialize = F3(function(size, offset, f)
+var _JsArray_initialize = F3(function(size, offset, func)
 {
     var result = new Array(size);
 
-    for (var i = 0; i < size; i++) {
-        result[i] = f(offset + i);
+    for (var i = 0; i < size; i++)
+    {
+        result[i] = func(offset + i);
     }
 
     return result;
@@ -32,7 +33,8 @@ var _JsArray_initializeFromList = F2(function (max, ls)
 {
     var result = new Array(max);
 
-    for (var i = 0; i < max && ls.$ !== '[]'; i++) {
+    for (var i = 0; i < max && ls.b; i++)
+    {
         result[i] = ls.a;
         ls = ls.b;
     }
@@ -41,84 +43,90 @@ var _JsArray_initializeFromList = F2(function (max, ls)
     return __Utils_Tuple2(result, ls);
 });
 
-var _JsArray_unsafeGet = F2(function(idx, arr)
+var _JsArray_unsafeGet = F2(function(index, array)
 {
-    return arr[idx];
+    return array[index];
 });
 
-var _JsArray_unsafeSet = F3(function(idx, val, arr)
+var _JsArray_unsafeSet = F3(function(index, value, array)
 {
-    var length = arr.length;
+    var length = array.length;
     var result = new Array(length);
 
-    for (var i = 0; i < length; i++) {
-        result[i] = arr[i];
+    for (var i = 0; i < length; i++)
+    {
+        result[i] = array[i];
     }
 
-    result[idx] = val;
+    result[index] = value;
     return result;
 });
 
-var _JsArray_push = F2(function(val, arr)
+var _JsArray_push = F2(function(value, array)
 {
-    var length = arr.length;
+    var length = array.length;
     var result = new Array(length + 1);
 
-    for (var i = 0; i < length; i++) {
-        result[i] = arr[i];
+    for (var i = 0; i < length; i++)
+    {
+        result[i] = array[i];
     }
 
-    result[length] = val;
+    result[length] = value;
     return result;
 });
 
-var _JsArray_foldl = F3(function(f, acc, arr)
+var _JsArray_foldl = F3(function(func, acc, array)
 {
-    var length = arr.length;
+    var length = array.length;
 
-    for (var i = 0; i < length; i++) {
-        acc = A2(f, arr[i], acc);
+    for (var i = 0; i < length; i++)
+    {
+        acc = A2(func, array[i], acc);
     }
 
     return acc;
 });
 
-var _JsArray_foldr = F3(function(f, acc, arr)
+var _JsArray_foldr = F3(function(func, acc, array)
 {
-    for (var i = arr.length - 1; i >= 0; i--) {
-        acc = A2(f, arr[i], acc);
+    for (var i = array.length - 1; i >= 0; i--)
+    {
+        acc = A2(func, array[i], acc);
     }
 
     return acc;
 });
 
-var _JsArray_map = F2(function(f, arr)
+var _JsArray_map = F2(function(func, array)
 {
-    var length = arr.length;
+    var length = array.length;
     var result = new Array(length);
 
-    for (var i = 0; i < length; i++) {
-        result[i] = f(arr[i]);
+    for (var i = 0; i < length; i++)
+    {
+        result[i] = func(array[i]);
     }
 
     return result;
 });
 
-var _JsArray_indexedMap = F3(function(f, offset, arr)
+var _JsArray_indexedMap = F3(function(func, offset, array)
 {
-    var length = arr.length;
+    var length = array.length;
     var result = new Array(length);
 
-    for (var i = 0; i < length; i++) {
-        result[i] = A2(f, offset + i, arr[i]);
+    for (var i = 0; i < length; i++)
+    {
+        result[i] = A2(func, offset + i, array[i]);
     }
 
     return result;
 });
 
-var _JsArray_slice = F3(function(from, to, arr)
+var _JsArray_slice = F3(function(from, to, array)
 {
-    return arr.slice(from, to);
+    return array.slice(from, to);
 });
 
 var _JsArray_appendN = F3(function(n, dest, source)
@@ -126,18 +134,21 @@ var _JsArray_appendN = F3(function(n, dest, source)
     var destLen = dest.length;
     var itemsToCopy = n - destLen;
 
-    if (itemsToCopy > source.length) {
+    if (itemsToCopy > source.length)
+    {
         itemsToCopy = source.length;
     }
 
     var size = destLen + itemsToCopy;
     var result = new Array(size);
 
-    for (var i = 0; i < destLen; i++) {
+    for (var i = 0; i < destLen; i++)
+    {
         result[i] = dest[i];
     }
 
-    for (var i = 0; i < itemsToCopy; i++) {
+    for (var i = 0; i < itemsToCopy; i++)
+    {
         result[i + destLen] = source[i];
     }
 
