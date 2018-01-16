be7216f8f8aecc06bb97f2e71c91156cf6de5d86
Evan Czaplicki <evancz@users.noreply.github.com>
2018-01-16 15:08:16 -0800

    Update Maybe for latest changes
    - Switch docs to use String.toFloat which gives a Maybe again.
    - Import Bool from Basics
    - Switch implementations away from using tuples
    - Add some functions for internal use only

--- src/Maybe.elm
+++ src/Maybe.elm
@@ -1,5 +1,5 @@
 module Maybe exposing
-  ( Maybe(Just,Nothing)
+  ( Maybe(..)
   , andThen
   , map, map2, map3, map4, map5
   , withDefault
@@ -17,6 +17,11 @@ you with optional arguments, error handling, and records with optional fields.
 # Chaining Maybes
 @docs andThen
 -}
+
+
+import Basics exposing (Bool(..))
+
+
 
 {-| Represent values that may or may not exist. It can be useful if you have a
 record field that is only filled in sometimes. Or if a function takes a value
@@ -56,13 +61,20 @@ withDefault default maybe =
 {-| Transform a `Maybe` value with a given function:
 
     map sqrt (Just 9) == Just 3
-    map sqrt Nothing == Nothing
+    map sqrt Nothing  == Nothing
+
+    map sqrt (String.toFloat "9") == Just 3
+    map sqrt (String.toFloat "x") == Nothing
+
 -}
 map : (a -> b) -> Maybe a -> Maybe b
 map f maybe =
-    case maybe of
-      Just value -> Just (f value)
-      Nothing -> Nothing
+  case maybe of
+    Just value ->
+      Just (f value)
+
+    Nothing ->
+      Nothing
 
 
 {-| Apply a function if all the arguments are `Just` a value.
@@ -70,48 +82,102 @@ map f maybe =
     map2 (+) (Just 3) (Just 4) == Just 7
     map2 (+) (Just 3) Nothing == Nothing
     map2 (+) Nothing (Just 4) == Nothing
+
+    map2 (+) (String.toInt "1") (String.toInt "123") == Just 124
+    map2 (+) (String.toInt "x") (String.toInt "123") == Nothing
+    map2 (+) (String.toInt "1") (String.toInt "1.3") == Nothing
 -}
 map2 : (a -> b -> value) -> Maybe a -> Maybe b -> Maybe value
 map2 func ma mb =
-    case (ma,mb) of
-      (Just a, Just b) ->
-          Just (func a b)
+  case ma of
+    Nothing ->
+      Nothing
 
-      _ ->
+    Just a ->
+      case mb of
+        Nothing ->
           Nothing
+
+        Just b ->
+          Just (func a b)
 
 
 {-|-}
 map3 : (a -> b -> c -> value) -> Maybe a -> Maybe b -> Maybe c -> Maybe value
 map3 func ma mb mc =
-    case (ma,mb,mc) of
-      (Just a, Just b, Just c) ->
-          Just (func a b c)
+  case ma of
+    Nothing ->
+      Nothing
 
-      _ ->
+    Just a ->
+      case mb of
+        Nothing ->
           Nothing
+
+        Just b ->
+          case mc of
+            Nothing ->
+              Nothing
+
+            Just c ->
+              Just (func a b c)
 
 
 {-|-}
 map4 : (a -> b -> c -> d -> value) -> Maybe a -> Maybe b -> Maybe c -> Maybe d -> Maybe value
 map4 func ma mb mc md =
-    case (ma,mb,mc,md) of
-      (Just a, Just b, Just c, Just d) ->
-          Just (func a b c d)
+  case ma of
+    Nothing ->
+      Nothing
 
-      _ ->
+    Just a ->
+      case mb of
+        Nothing ->
           Nothing
+
+        Just b ->
+          case mc of
+            Nothing ->
+              Nothing
+
+            Just c ->
+              case md of
+                Nothing ->
+                  Nothing
+
+                Just d ->
+                  Just (func a b c d)
 
 
 {-|-}
 map5 : (a -> b -> c -> d -> e -> value) -> Maybe a -> Maybe b -> Maybe c -> Maybe d -> Maybe e -> Maybe value
 map5 func ma mb mc md me =
-    case (ma,mb,mc,md,me) of
-      (Just a, Just b, Just c, Just d, Just e) ->
-          Just (func a b c d e)
+  case ma of
+    Nothing ->
+      Nothing
 
-      _ ->
+    Just a ->
+      case mb of
+        Nothing ->
           Nothing
+
+        Just b ->
+          case mc of
+            Nothing ->
+              Nothing
+
+            Just c ->
+              case md of
+                Nothing ->
+                  Nothing
+
+                Just d ->
+                  case me of
+                    Nothing ->
+                      Nothing
+
+                    Just e ->
+                      Just (func a b c d e)
 
 
 {-| Chain together many computations that may fail. It is helpful to see its
@@ -127,25 +193,24 @@ definition:
                 Nothing
 
 This means we only continue with the callback if things are going well. For
-example, say you need to use (`head : List Int -> Maybe Int`) to get the
-first month from a `List` and then make sure it is between 1 and 12:
+example, say you need to parse some user input as a month:
+
+    parseMonth : String -> Maybe Int
+    parseMonth userInput =
+        String.toInt userInput
+          |> andThen toValidMonth
 
     toValidMonth : Int -> Maybe Int
     toValidMonth month =
-        if month >= 1 && month <= 12 then
+        if 1 <= month && month <= 12 then
             Just month
         else
             Nothing
 
-    getFirstMonth : List Int -> Maybe Int
-    getFirstMonth months =
-        head months
-          |> andThen toValidMonth
-
-If `head` fails and results in `Nothing` (because the `List` was `empty`),
-this entire chain of operations will short-circuit and result in `Nothing`.
-If `toValidMonth` results in `Nothing`, again the chain of computations
-will result in `Nothing`.
+In the `parseMonth` function, if `String.toInt` produces `Nothing` (because
+the `userInput` was not an integer) this entire chain of operations will
+short-circuit and result in `Nothing`. If `toValidMonth` results in `Nothing`,
+again the chain of computations will result in `Nothing`.
 -}
 andThen : (a -> Maybe b) -> Maybe a -> Maybe b
 andThen callback maybeValue =
@@ -155,3 +220,29 @@ andThen callback maybeValue =
 
         Nothing ->
             Nothing
+
+
+
+-- FOR INTERNAL USE ONLY
+--
+-- Use `case` expressions for this in Elm code!
+
+
+isJust : Maybe a -> Bool
+isJust maybe =
+  case maybe of
+    Just _ ->
+      True
+
+    Nothing ->
+      False
+
+
+destruct : b -> (a -> b) -> Maybe a -> b
+destruct default func maybe =
+  case maybe of
+    Just a ->
+      func a
+
+    Nothing ->
+      default
