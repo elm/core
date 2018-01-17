94c22e51bba811c820ed44f26e1b02380917923b
Evan Czaplicki <evancz@users.noreply.github.com>
2018-01-16 20:01:35 -0800

    Use better names in Set module

--- src/Set.elm
+++ src/Set.elm
@@ -56,113 +56,123 @@ empty =
 {-| Create a set with one value.
 -}
 singleton : comparable -> Set comparable
-singleton k =
-  Set_elm_builtin <| Dict.singleton k ()
+singleton key =
+  Set_elm_builtin <|
+    Dict.singleton key ()
 
 
 {-| Insert a value into a set.
 -}
 insert : comparable -> Set comparable -> Set comparable
-insert k (Set_elm_builtin d) =
-  Set_elm_builtin <| Dict.insert k () d
+insert key (Set_elm_builtin dict) =
+  Set_elm_builtin <|
+    Dict.insert key () dict
 
 
 {-| Remove a value from a set. If the value is not found, no changes are made.
 -}
 remove : comparable -> Set comparable -> Set comparable
-remove k (Set_elm_builtin d) =
-  Set_elm_builtin <| Dict.remove k d
+remove key (Set_elm_builtin dict) =
+  Set_elm_builtin <|
+    Dict.remove key dict
 
 
 {-| Determine if a set is empty.
 -}
 isEmpty : Set a -> Bool
-isEmpty (Set_elm_builtin d) =
-  Dict.isEmpty d
+isEmpty (Set_elm_builtin dict) =
+  Dict.isEmpty dict
 
 
 {-| Determine if a value is in a set.
 -}
 member : comparable -> Set comparable -> Bool
-member k (Set_elm_builtin d) =
-  Dict.member k d
+member key (Set_elm_builtin dict) =
+  Dict.member key dict
 
 
 {-| Determine the number of elements in a set.
 -}
 size : Set a -> Int
-size (Set_elm_builtin d) =
-  Dict.size d
+size (Set_elm_builtin dict) =
+  Dict.size dict
 
 
 {-| Get the union of two sets. Keep all values.
 -}
 union : Set comparable -> Set comparable -> Set comparable
-union (Set_elm_builtin d1) (Set_elm_builtin d2) =
-  Set_elm_builtin <| Dict.union d1 d2
+union (Set_elm_builtin dict1) (Set_elm_builtin dict2) =
+  Set_elm_builtin <|
+    Dict.union dict1 dict2
 
 
 {-| Get the intersection of two sets. Keeps values that appear in both sets.
 -}
 intersect : Set comparable -> Set comparable -> Set comparable
-intersect (Set_elm_builtin d1) (Set_elm_builtin d2) =
-  Set_elm_builtin <| Dict.intersect d1 d2
+intersect (Set_elm_builtin dict1) (Set_elm_builtin dict2) =
+  Set_elm_builtin <|
+    Dict.intersect dict1 dict2
 
 
 {-| Get the difference between the first set and the second. Keeps values
 that do not appear in the second set.
 -}
 diff : Set comparable -> Set comparable -> Set comparable
-diff (Set_elm_builtin d1) (Set_elm_builtin d2) =
-  Set_elm_builtin <| Dict.diff d1 d2
+diff (Set_elm_builtin dict1) (Set_elm_builtin dict2) =
+  Set_elm_builtin <|
+    Dict.diff dict1 dict2
 
 
 {-| Convert a set into a list, sorted from lowest to highest.
 -}
 toList : Set a -> List a
-toList (Set_elm_builtin d) =
-  Dict.keys d
+toList (Set_elm_builtin dict) =
+  Dict.keys dict
 
 
 {-| Convert a list into a set, removing any duplicates.
 -}
 fromList : List comparable -> Set comparable
-fromList xs = List.foldl insert empty xs
+fromList list =
+  List.foldl insert empty list
 
 
 {-| Fold over the values in a set, in order from lowest to highest.
 -}
 foldl : (a -> b -> b) -> b -> Set a -> b
-foldl f b (Set_elm_builtin d) =
-  Dict.foldl (\k _ b -> f k b) b d
+foldl func initialState (Set_elm_builtin dict) =
+  Dict.foldl (\key _ state -> func key state) initialState dict
 
 
 {-| Fold over the values in a set, in order from highest to lowest.
 -}
 foldr : (comparable -> b -> b) -> b -> Set comparable -> b
-foldr f b (Set_elm_builtin d) =
-  Dict.foldr (\k _ b -> f k b) b d
+foldr func initialState (Set_elm_builtin dict) =
+  Dict.foldr (\key _ state -> func key state) initialState dict
 
 
 {-| Map a function onto a set, creating a new set with no duplicates.
 -}
 map : (comparable -> comparable2) -> Set comparable -> Set comparable2
-map f s = fromList (List.map f (toList s))
+map func set =
+  fromList (List.map func (toList set))
 
 
 {-| Create a new set consisting only of elements which satisfy a predicate.
 -}
 filter : (comparable -> Bool) -> Set comparable -> Set comparable
-filter p (Set_elm_builtin d) =
-  Set_elm_builtin <| Dict.filter (\k _ -> p k) d
+filter isGood (Set_elm_builtin dict) =
+  Set_elm_builtin <|
+    Dict.filter (\key _ -> isGood key) dict
 
 
 {-| Create two new sets; the first consisting of elements which satisfy a
 predicate, the second consisting of elements which do not.
 -}
 partition : (comparable -> Bool) -> Set comparable -> (Set comparable, Set comparable)
-partition p (Set_elm_builtin d) =
+partition isGood (Set_elm_builtin dict) =
   let
-    (p1, p2) = Dict.partition (\k _ -> p k) d
+    (dict1, dict2) =
+      Dict.partition (\key _ -> isGood key) dict
   in
-    (Set_elm_builtin p1, Set_elm_builtin p2)
+    (Set_elm_builtin dict1, Set_elm_builtin dict2)
