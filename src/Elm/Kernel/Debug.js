0933b6c0e0fbf50635e7995a626d12d22637a0a4
Evan Czaplicki <evancz@users.noreply.github.com>
2019-09-24 16:24:08 -0400

    use dull cyan for <function> and <internal> values in REPL

--- src/Elm/Kernel/Debug.js
+++ src/Elm/Kernel/Debug.js
@@ -218,7 +218,7 @@ function _Debug_fadeColor(ansi, string)
 
 function _Debug_internalColor(ansi, string)
 {
-	return ansi ? '\x1b[94m' + string + '\x1b[0m' : string;
+	return ansi ? '\x1b[36m' + string + '\x1b[0m' : string;
 }
 
 function _Debug_toHexDigit(n)
